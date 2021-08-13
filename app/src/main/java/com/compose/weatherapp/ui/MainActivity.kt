a356f20ea63a019d3156715808090399df035286
Pari <parivendhan@nimblewireless.com>
2021-08-13 08:45:03 +0530

    Init

--- app/src/main/java/com/compose/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/compose/weatherapp/ui/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initValues()
-        locationRequest()
         observers()
 
 
@@ -74,8 +73,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun locationRequest() {
 
-        /* val request = LocationSettingsRequest.Builder()
-         val settingsClient = LocationServices.getSettingsClient(this)*/
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         if (Utils.checkLocationPermission(this)) {
@@ -90,17 +87,6 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-/*
-        locationRequest = LocationRequest.create().apply {
-            interval = 60000L
-            fastestInterval = 1000L
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
-
-        request.addLocationRequest(locationRequest)
-        val response = settingsClient.checkLocationSettings(request.build())
-        response.addOnSuccessListener { startLocationUpdated() }
-            .addOnFailureListener { Log.d(TAG, "locationRequest: $it") }*/
 
 
     }
@@ -122,46 +108,15 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onStop() {
-        super.onStop()
-        stopLocationUpdated()
-    }
-
     private fun checkPermission() {
         if (Utils.checkLocationPermission(this)) {
-            startLocationUpdated()
+            locationRequest()
         } else {
             Utils.requestLocationPermission(this)
         }
     }
 
-    /*  private val locationCallback = object : LocationCallback() {
-          override fun onLocationResult(locations: LocationResult?) {
-              super.onLocationResult(locations)
-              locations?.let {
-                  it.locations.forEach { location ->
 
-                  }
-              }
-          }
-      }*/
-
-
-    private fun startLocationUpdated() {
-
-        /* if (Utils.checkLocationPermission(this)) {
-             fusedLocationProviderClient.requestLocationUpdates(
-                 locationRequest,
-                 locationCallback,
-                 Looper.getMainLooper()
-             )
-
-         }*/
-    }
-
-    private fun stopLocationUpdated() {
-//        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
-    }
 
 
 }
